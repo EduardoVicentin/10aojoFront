c295dcddd2b30db2ae8fe92f8cc235a0762d6617
Vinícius de Andrade Barros <viniciusdeandrade96@gmail.com>
2023-02-11 15:28:43 -0300

    Alterações Home page

--- lib/controller/assists_controller.dart
+++ lib/controller/assists_controller.dart
@@ -7,15 +7,53 @@ import 'package:get/get.dart';
 class AssistsController extends GetxController with StateMixin<List<Assist>>
 {
   late AssistService _assistService;
-  
+  List<Assist> allAssists = [];
+  List<Assist> selectedAssists = [];
   @override
   void onInit(){
     super.onInit();
     _assistService = Get.find<AssistService>();
+    getAssistList();
   }
   
+  @override
+  void onReady(){
+    super.onReady();
+    selectedAssists = Get.arguments;
+  }
+
+  void finishSelectAssist(){
+    Get.back();
+  }
+
+  bool isSelected (index){
+     Assist assist = allAssists[index];
+     int indexFound = selectedAssists.indexWhere((element) => element.id == assist.id);
+    if(indexFound == -1){
+      return false;
+    }
+      return true;
+    
+  }
+
+  void selectAssists(int index ){ 
+    Assist assist = allAssists[index];
+    int indexFound = selectedAssists.indexWhere((element) => element.id == assist.id);
+    if(indexFound == -1){
+      selectedAssists.add(assist);
+    }
+    else{
+      selectedAssists.removeAt(indexFound);
+    }
+    change(allAssists, status: RxStatus.success());
+  }
+
     void getAssistList(){
-      _assistService.getAssists().then(((value) => change(value, status: RxStatus.success()))).onError((error, stackTrace) => change([],status: RxStatus.error(error.toString())));
+      _assistService.getAssists().then((value) {
+      allAssists.addAll(value);
+      change(value, status: RxStatus.success());}
+      ).onError((error, stackTrace) { change([],status: RxStatus.error(error.toString()));
+      });
     }
 
 }
